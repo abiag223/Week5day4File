d89f53d3bb21bf4d1447b25de6cc11674afebf0f
abiag223 <abigail.aquinogarcia2934@gmail.com>
2024-08-14 18:49:52 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// Select all elements with the class 'navItem' and change color to white
+ $('.navItem').css('color', 'white');
+ 
+ 
+ // Select the element with the ID 'nav' and change background color to #145 
+ $('#nav').css('background-color', '#145'); 
+ 
+ // Select all even list items and change background color to #ccc
+  $('ul li:even').css('background-color', '#ccc');
+  
+  // Select all odd list items and change background color to #aaa 
+  
+  $('ul li:odd').css('background-color', '#aaa'); 
+  
+  // Select the input tag and change the value to your first name 
+  $('#myInput').val('Your First Name');
